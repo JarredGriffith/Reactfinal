c52a607f9aaad8c7563fcdd4de2ae2aaa86dde5f
jarre <jarredgriffith29@gmail.com>
2021-03-22 23:09:44 -0700

    adding API calls

---
+++ final-project/src/services/apiservices.js
@@ -0,0 +1,45 @@
+const ENDPOINT = "https://crudcrud.com/api/5c82594d1f594441a2f62f4a4a228ad1/unicorns"; 
+
+class APIcalls {
+    get = async ()=> {
+        try {
+            //making a get requet
+            const resp = await fetch(`${ENDPOINT}`); 
+            const data = await resp.json(); 
+            return data; 
+        } catch(e) {
+            console.log("Error with the get request",e)
+        }
+    }
+
+    create = async(car) => {
+        console.log(JSON.stringify(car))
+        try{
+            const resp = await fetch(ENDPOINT,{
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(car)
+            }); 
+            return resp; 
+        } catch(e) {
+            console.error(`Problem creating employee: ${e}`);
+        }
+    }
+    delete = async(id) => {
+        try {
+            const resp = await fetch(`${ENDPOINT}/${id}`, {
+                method: 'DELETE',
+                headers: {
+                  'Content-Type': 'application/json'
+                }
+            });
+            return resp;
+        } catch(e) {
+            console.error(`Problem deleting employee:${e}`);
+        }
+    }
+}
+
+export const Apicalls = new APIcalls(); 
